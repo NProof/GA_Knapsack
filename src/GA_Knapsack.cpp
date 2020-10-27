0d0b1647db1a716a852133bd14f7a64c93b04b72
NProof <34260623+NProof@users.noreply.github.com>
2020-10-27 11:59:28 +0800

    reduce

--- src/GA_Knapsack.cpp
+++ src/GA_Knapsack.cpp
@@ -3,12 +3,5 @@
 
 int main() {
     Problem p;
-    std::cout << p.W << "\n";
-    double sum = 0;
-    for(auto item : p.items) {
-        sum += item.getw();
-        std::cout << item.getw() << " and "  << item.getp() << "\n";
-    }
-    std::cout << sum << "\n";
     return 0;
 }
